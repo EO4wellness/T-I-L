721bf46e777ab7d633930bf243a5f99ffecb011b
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2020-12-31 18:51:15 -0600

    comments in PHP

---
+++ PHP/Comments.php
@@ -0,0 +1,43 @@
+/* In PHP code, a comment is a line that is not executed as part of the program. 
+# You can use comments to communicate to others so they understand what you're doing, 
+# or as a reminder to yourself of what you did. 
+# 
+# You can have as many, or as few, comments to your code as you desire. 
+# 
+# A single-line commment starts with "//" 
+# 
+# For Example: 
+*/
+
+<?php   
+    echo "<p>Hello World!</p>";
+    // This is a single-line comment
+    echo "<p>I am learning PHP!</p>";
+    echo "<p>This is my first program!</p>";
+?>
+
+/*
+#
+# Quiz:  Which of the following characters indicates comments within the PHP syntax?
+#        A) //  B)  *** C) <!____
+#
+# Correct Answer is "A) //
+#
+# Multi-line comments are used for composing comments that take more than a single line.
+# A multi-line comment begins with /* and ends with */. 
+# 
+# Adding comments as you write your code is a good practice. It helps others understand your thinking and makes it easier for you to recall your thought processes when you refer to your code later on.
+# 
+*/
+
+<?php   
+   echo "<p>Hello World!</p>";
+    /*
+    This is a multi-line comment block
+    that spans over
+    multiple lines
+    */
+    echo "<p>I am learning PHP!</p>";
+    echo "<p>This is my first program!</p>";
+?>
+
