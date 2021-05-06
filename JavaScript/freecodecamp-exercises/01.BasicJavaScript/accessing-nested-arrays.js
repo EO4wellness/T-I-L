184bb637081e2b32e40b6f9e721cfcc28040d230
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-06 13:08:12 -0500

    Create accessing-nested-arrays.js

---
+++ JavaScript/freecodecamp-exercises/01.BasicJavaScript/accessing-nested-arrays.js
@@ -0,0 +1,20 @@
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1];
