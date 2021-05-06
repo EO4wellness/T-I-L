f9618bf31b225d466dbd3eaa7b8931778deab626
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-06 13:09:14 -0500

    Create delete-properties-from-a-javascript-object.js

---
+++ JavaScript/freecodecamp-exercises/01.BasicJavaScript/delete-properties-from-a-javascript-object.js
@@ -0,0 +1,11 @@
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
