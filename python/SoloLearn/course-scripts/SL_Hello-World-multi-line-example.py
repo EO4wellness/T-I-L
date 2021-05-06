4ded1cd371ac5a28406c037762405b89f80656e9
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-06 14:10:47 -0500

    Delete SL_Hello-World-multi-line-example.py

--- python/SoloLearn/course-scripts/SL_Hello-World-multi-line-example.py
+++
@@ -1,6 +0,0 @@
-# How to print multiple lines python Script from the SoloLearn Course
-print('Hello world!')
-print('Γειά σου Κόσμε!')
-print('こんにちは世界')
-print('Hola Mundo!')
-print('Spam and eggs...')
