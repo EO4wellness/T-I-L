9ebe2daa61b0e133b91283fa61ab2c1cadde7161
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-06 13:06:05 -0500

    Create SL_Hello-World-multi-line-example.py

---
+++ python/SoloLearn/course-scripts/SL_Hello-World-multi-line-example.py
@@ -0,0 +1,6 @@
+# How to print multiple lines python Script from the SoloLearn Course
+print('Hello world!')
+print('Γειά σου Κόσμε!')
+print('こんにちは世界')
+print('Hola Mundo!')
+print('Spam and eggs...')
