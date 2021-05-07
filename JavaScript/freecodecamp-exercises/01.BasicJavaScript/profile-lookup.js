9e672334688e6df1d92a0284a666734b36747adb
EO4wellness <eo4wellness1122617@gmail.com>
2021-05-07 00:22:21 -0500

    Create profile-lookup.js

---
+++ JavaScript/freecodecamp-exercises/01.BasicJavaScript/profile-lookup.js
@@ -0,0 +1,41 @@
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName ===name) {
+	  return contacts[i][prop] || "No such property";
+	}
+  }
+  return "No such contact";  
+// Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
