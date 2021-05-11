55d796e01f798dded934cd47b519f2d2e1336467
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-10 19:24:11 -0500

    Create use-recursion-to-create-a-countdown.js

---
+++ JavaScript/freecodecamp-exercises/01.BasicJavaScript/use-recursion-to-create-a-countdown.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+// Only change code above this line
