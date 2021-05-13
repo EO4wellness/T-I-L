6ac621159c4f329bbb2eef098a456ca70f3ce7a4
EO4wellness <eo4wellness1122617@gmail.com>
2021-05-12 19:35:55 -0500

    Create declare-a-read-only-variable-with-the-const-keyword.js

---
+++ JavaScript/freecodecamp-exercises/01.BasicJavaScript/declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,13 @@
+function printManyTimes(str) {
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
