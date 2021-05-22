885d47712721095d74c79cee3cba4aec083a839f
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-22 12:03:10 -0500

    Create 18_write-concise-object-literal-declarations-using-object-property-shorthand.js

---
+++ JavaScript/freecodecamp-exercises/02.ES6/18_write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,10 @@
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  "use strict";
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
