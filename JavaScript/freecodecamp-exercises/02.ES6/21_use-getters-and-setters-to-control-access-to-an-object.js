ba719f6ab0199acd5616e1754c42206a0ce9fd0e
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-05-25 00:27:21 -0500

    Create 21_use-getters-and-setters-to-control-access-to-an-object.js

---
+++ JavaScript/freecodecamp-exercises/02.ES6/21_use-getters-and-setters-to-control-access-to-an-object.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
