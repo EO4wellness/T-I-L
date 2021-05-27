1417859530ff689a254ebad07229673be23668ea
eo4wellness <eo4wellness1122617@gmail.com>
2021-05-27 14:04:49 -0500

    Create 23_use-export-to-share-a-code-block.js

---
+++ JavaScript/freecodecamp-exercises/02.ES6/23_use-export-to-share-a-code-block.js
@@ -0,0 +1,10 @@
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export { lowercaseString };
+
