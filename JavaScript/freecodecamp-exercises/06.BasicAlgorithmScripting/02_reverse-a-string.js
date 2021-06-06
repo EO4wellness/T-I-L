a0437e502154df91125b5dd5c454de06486502a0
eo4wellness <eo4wellness1122617@gmail.com>
2021-06-06 15:46:44 -0500

    Create 02_reverse-a-string.js

---
+++ JavaScript/freecodecamp-exercises/06.BasicAlgorithmScripting/02_reverse-a-string.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+reverseString("hello");
