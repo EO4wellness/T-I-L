ff7073986f1ce050f81c251c76207bb29820700d
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-06-06 09:28:03 -0500

    Create ASCII-value-calculator-code.js

---
+++ JavaScript/ASCII-value-calculator-code.js
@@ -0,0 +1,8 @@
+// Pick a string. Your string can have any number of characters.
+var my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+var ASCII_value = my_string.charCodeAt(0);
+
+//  print the value
+console.log(ASCII_value)
