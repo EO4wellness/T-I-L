a29054d539c29669bd157ea11e908f00a4647d91
eo4wellness <eo4wellness1122617@gmail.com>
2021-07-04 12:44:15 -0500

    Create 17_check-if-an-object-has-a-property.js

---
+++ JavaScript/freecodecamp-exercises/05.BasicDataStructures/17_check-if-an-object-has-a-property.js
@@ -0,0 +1,31 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(obj) {
+  if (
+    obj.hasOwnProperty("Alan") &&
+    obj.hasOwnProperty("Jeff") &&
+    obj.hasOwnProperty("Sarah") &&
+    obj.hasOwnProperty("Ryan")
+  ) {
+    return true;
+  }
+  return false;
+}
+console.log(isEveryoneHere(users));
