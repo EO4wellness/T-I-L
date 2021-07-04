068c71b97362f1764cef9591fef66d0b7fbd499e
eo4wellness <eo4wellness1122617@gmail.com>
2021-07-04 15:15:17 -0500

    Create 18_iterate-through-the-keys-of-an-object-with-a-for---in-statement.js

---
+++ JavaScript/freecodecamp-exercises/05.BasicDataStructures/18_iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -0,0 +1,11 @@
+function countOnline(usersObj) {
+  // Only change code below this line
+  let result = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+  // Only change code above this line
+}
