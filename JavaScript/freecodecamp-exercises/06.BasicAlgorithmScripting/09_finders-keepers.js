d33ef0fb8ed139c42f1d9ccaf99b5222b51eda0f
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-07-07 13:50:39 -0500

    Create 09_finders-keepers.js

---
+++ JavaScript/freecodecamp-exercises/06.BasicAlgorithmScripting/09_finders-keepers.js
@@ -0,0 +1,13 @@
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
+findElement([1, 2, 3, 4], num => num % 2 === 0);
