30d58d6fae97c375dd7553f4b84faabc6591d2e2
eo4wellness <eo4wellness1122617@gmail.com>
2021-07-08 13:45:39 -0500

    Delete 09_title-case-a-sentence.js

--- JavaScript/freecodecamp-exercises/06.BasicAlgorithmScripting/09_title-case-a-sentence.js
+++
@@ -1,9 +0,0 @@
-function titleCase(str) {
-  var convertToArray = str.toLowerCase().split(" ");
-  var result = convertToArray.map(function(val) {
-    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
-  });
-  return result.join(" ");
-}
-
-titleCase("I'm a little tea pot");
