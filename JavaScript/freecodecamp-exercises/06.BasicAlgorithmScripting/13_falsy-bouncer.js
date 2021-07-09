f60bfd6062d87cc981b4da031fabf65ebc11dd0f
eo4wellness <eo4wellness1122617@gmail.com>
2021-07-09 15:31:15 -0500

    Create 13_falsy-bouncer.js

---
+++ JavaScript/freecodecamp-exercises/06.BasicAlgorithmScripting/13_falsy-bouncer.js
@@ -0,0 +1,8 @@
+function bouncer(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+bouncer([7, "ate", "", false, 9]);
