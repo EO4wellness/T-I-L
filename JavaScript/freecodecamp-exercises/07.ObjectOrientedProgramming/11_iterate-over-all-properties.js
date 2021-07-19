00d99c334be0ce23895225582d1d2c01dfdc46f8
eo4wellness <eo4wellness1122617@gmail.com>
2021-07-19 10:54:42 -0500

    Create 11_iterate-over-all-properties.js

---
+++ JavaScript/freecodecamp-exercises/07.ObjectOrientedProgramming/11_iterate-over-all-properties.js
@@ -0,0 +1,19 @@
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+for (let property in beagle) {
+  if (Dog.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
