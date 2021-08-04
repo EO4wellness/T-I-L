ffb1faa1b85b6440384466c0d5557b031b80e3e7
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-08-04 00:15:56 -0500

    Create 04_avoid-mutations-and-side-effects-using-functional-programming.js

---
+++ JavaScript/freecodecamp-exercises/08.FunctionalProgramming/04_avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,14 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+// Using the increment operator (++) on fixedValue 
+// will mutate fixedValue, meaning 
+// it will no longer reference the same value
+// it was assigned with.
+
+  return fixedValue + 1;
+
+  // Only change code above this line
+}
