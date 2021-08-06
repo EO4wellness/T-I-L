c17e187a54a4613f4a57d82b8de6918b0b339d7f
eo4wellness <eo4wellness1122617@gmail.com>
2021-08-06 13:44:14 -0500

    Create 06_refactor-global-variables-out-of-functions.js

---
+++ JavaScript/freecodecamp-exercises/08.FunctionalProgramming/06_refactor-global-variables-out-of-functions.js
@@ -0,0 +1,22 @@
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before bookName
+
+function add(list, bookName) {
+  return [...list, bookName];
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+function remove(list, bookName) {
+  return list.filter(book => book !== bookName);
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
