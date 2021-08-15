4382c721d844d73b043163c869c25c7f42e7f8ae
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-08-15 18:55:31 -0500

    Create 14_add-elements-to-the-end-of-an-array-using-concat-instead-of-push.js

---
+++ JavaScript/freecodecamp-exercises/08.FunctionalProgramming/14_add-elements-to-the-end-of-an-array-using-concat-instead-of-push.js
@@ -0,0 +1,9 @@
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
