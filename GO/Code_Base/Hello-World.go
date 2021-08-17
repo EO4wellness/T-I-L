a17048ad44b1799954cc5a917ba0b7f10408a9d4
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-08-17 15:39:11 -0500

    first GO code
    https://www.learn-golang.org/

---
+++ GO/Code_Base/Hello-World.go
@@ -0,0 +1,12 @@
+// Welcome to the Interactive Go Tutorial.
+// Start by choosing a chapter, write your code in this window.
+// the link to this resource source is: https://www.learn-golang.org/ 
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+}
