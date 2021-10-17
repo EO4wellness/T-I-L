c7ebb754cbba0ab542e6843dd75f1d71eadd3a4b
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-10-17 14:22:14 -0500

    Create 26_use-state-to-toggle-an-element.jsx

---
+++ FrontEnd/freeCodeCamp/exercises/React/26_use-state-to-toggle-an-element.jsx
@@ -0,0 +1,38 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // Change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  toggleVisibility() {
+    this.setState(state => {
+      if (state.visibility === true) {
+         return { visibility: false };
+       } else {
+         return { visibility: true };
+      }
+    });
+  }
+  // Change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+}
